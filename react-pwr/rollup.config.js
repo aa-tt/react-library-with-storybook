988cc36da84a97ab82631535db411ef894fbe70d
anunay2021 <anunay.anindya@khoros.com>
2021-06-19 18:30:46 +0530

    config for types .d.ts

--- react-pwr/rollup.config.js
+++ react-pwr/rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
 import postcss from "rollup-plugin-postcss";
+import typescript from "rollup-plugin-typescript2";
 
 export default [
   {
@@ -17,8 +18,16 @@ export default [
         format: "es",
         exports: "named",
       },
+      {
+        file: "dist/index.d.ts",
+        format: "es",
+        exports: "named",
+      },
     ],
     plugins: [
+      typescript({
+        typescript: require("typescript"),
+      }),
       postcss({
         plugins: [],
         minimize: true,
